b90d5b61b08766bce5638272911a16e69684c502
filipe <filipe.dias09@hotmail.com>
2022-10-12 13:09:40 -0300

    background da page #fafafa

--- cadastro.js
+++ cadastro.js
@@ -4,5 +4,10 @@ let usuario = document.querySelector('.user')
 usuario.innerHTML = '<strong> Nome Usuario </strong>'
 */
 
+/* Alterando nomenclatura placeholder name
+document.getElementsByName('name')[0].placeholder='Digite Primeiro Nome';
+*/
 
-document.getElementsByName('name')[0].placeholder='Primeiro Nome';
+let color = document.querySelector('body')
+
+color.style.backgroundColor = '#fafafa'
