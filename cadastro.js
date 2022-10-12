07b0856f6bbbe53880ed7f182ed563545d46a797
filipe <filipe.dias09@hotmail.com>
2022-10-12 12:40:35 -0300

    alterando nomenclatura do placeholder name

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,8 @@
+/*
 let usuario = document.querySelector('.user')
 
 usuario.innerHTML = '<strong> Nome Usuario </strong>'
+*/
+
+
+document.getElementsByName('name')[0].placeholder='Primeiro Nome';
