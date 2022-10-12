fc5b64ae47e4f59c51a22d5b25d3fdea09761141
filipe <filipe.dias09@hotmail.com>
2022-10-12 13:17:03 -0300

    alterando text placeholder

--- cadastro.js
+++ cadastro.js
@@ -1,13 +1,11 @@
-/*
-let usuario = document.querySelector('.user')
+// Nome do usuario em negrito
+let usuario = document.querySelector('.user');
+usuario.innerHTML = '<strong> Nome Usuario </strong>';
 
-usuario.innerHTML = '<strong> Nome Usuario </strong>'
-*/
+// Alterando nomenclatura placeholder name
+document.getElementsByName('name')[0].placeholder='Digite seu primeiro nome'
 
-/* Alterando nomenclatura placeholder name
-document.getElementsByName('name')[0].placeholder='Digite Primeiro Nome';
-*/
+// Alterado background da page
+let color = document.querySelector('body');
+color.style.backgroundColor = '#fafafa';
 
-let color = document.querySelector('body')
-
-color.style.backgroundColor = '#fafafa'
